53c436d3b6b76ec252454a18baea549b84095e3d
Alexey Orlov <m110h@mail.ru>
2025-11-08 02:18:23 +0200

    первый коммит

---
+++ fcsnstoragesdk.go
@@ -0,0 +1,191 @@
+package fcsnstoragesdk
+
+import (
+	"context"
+	"time"
+
+	pb "github.com/m110h/fcsn-proto/gen/storage"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+/* Some description */
+type Connection struct {
+	Connection *grpc.ClientConn
+}
+
+/* Some description */
+func (c *Connection) Connect(target string, isInsecure bool) error {
+	var opts []grpc.DialOption
+	if isInsecure {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+	conn, err := grpc.NewClient(target, opts...)
+	if err != nil {
+		return err
+	}
+	c.Connection = conn
+	return nil
+}
+
+/* Some description */
+func (c *Connection) Close() {
+	c.Connection.Close()
+}
+
+/* Some description */
+type Client struct {
+	client pb.FamilyRootsStorageClient
+}
+
+/* Some description */
+func NewClient(conn *Connection) *Client {
+	client := Client{
+		client: pb.NewFamilyRootsStorageClient(conn.Connection),
+	}
+	return &client
+}
+
+/* Some description */
+func NewConnectionAndClient(target string, isInsecure bool) (*Connection, *Client, error) {
+	var conn Connection
+
+	err := conn.Connect(target, isInsecure)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cli := NewClient(&conn)
+
+	return &conn, cli, nil
+}
+
+/* Some description */
+func (c *Client) SetChart(ctx context.Context, uid, chart string) (time.Time, error) {
+	req := &pb.SetChartRequest{
+		Uid:   uid,
+		Chart: chart,
+	}
+	r, err := c.client.SetChart(ctx, req)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	return r.UpdatedAt.AsTime(), nil
+}
+
+/* Some description */
+func (c *Client) GetChart(ctx context.Context, uid string) (string, error) {
+	req := &pb.GetChartRequest{
+		Uid: uid,
+	}
+
+	res, err := c.client.GetChart(ctx, req)
+	if err != nil {
+		return "", err
+	}
+
+	return res.Chart, nil
+}
+
+/* Some description */
+func (c *Client) Subscribe(ctx context.Context, source_uid, target_uid string) (time.Time, error) {
+	req := &pb.SubscribeRequest{
+		SourceUid: source_uid,
+		TargetUid: target_uid,
+	}
+
+	r, err := c.client.Subscribe(ctx, req)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	return r.CreatedAt.AsTime(), nil
+}
+
+/* Some description */
+func (c *Client) Unsubscribe(ctx context.Context, source_uid, target_uid string) (time.Time, error) {
+	req := &pb.UnsubscribeRequest{
+		SourceUid: source_uid,
+		TargetUid: target_uid,
+	}
+
+	r, err := c.client.Unsubscribe(ctx, req)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	return r.DeletedAt.AsTime(), nil
+}
+
+/* Some description */
+func (c *Client) IsSubscriber(ctx context.Context, target_uid, source_uid string) (bool, error) {
+	req := &pb.IsSubscriberRequest{
+		TargetUid: target_uid,
+		SourceUid: source_uid,
+	}
+
+	r, err := c.client.IsSubscriber(ctx, req)
+	if err != nil {
+		return false, err
+	}
+
+	return r.IsSubscriber, nil
+}
+
+/* Some description */
+func (c *Client) GetSubscribersCount(ctx context.Context, uid string) (int64, error) {
+	req := &pb.GetSubscribersCountRequest{
+		Uid: uid,
+	}
+
+	r, err := c.client.GetSubscribersCount(ctx, req)
+	if err != nil {
+		return 0, err
+	}
+
+	return r.Count, nil
+}
+
+/* Some description */
+func (c *Client) GetSubscribers(ctx context.Context, uid string) ([]string, error) {
+	req := &pb.GetSubscribersRequest{
+		Uid: uid,
+	}
+
+	r, err := c.client.GetSubscribers(ctx, req)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return r.Subscribers, nil
+}
+
+/* Some description */
+func (c *Client) GetSubscribtionsCount(ctx context.Context, uid string) (int64, error) {
+	req := &pb.GetSubscribtionsCountRequest{
+		Uid: uid,
+	}
+
+	r, err := c.client.GetSubscribtionsCount(ctx, req)
+	if err != nil {
+		return 0, err
+	}
+
+	return r.Count, nil
+}
+
+/* Some description */
+func (c *Client) GetSubscribtions(ctx context.Context, uid string) ([]string, error) {
+	req := &pb.GetSubscribtionsRequest{
+		Uid: uid,
+	}
+
+	r, err := c.client.GetSubscribtions(ctx, req)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return r.Subscribtions, nil
+}
---
+++ fcsnstoragesdk_test.go
@@ -0,0 +1,108 @@
+package fcsnstoragesdk
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func SetChart(client *Client, uid, chart string) (time.Time, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return client.SetChart(ctx, uid, chart)
+}
+
+func GetChart(client *Client, uid string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return client.GetChart(ctx, uid)
+}
+
+func Subscribe(client *Client, source_uid, target_uid string) (time.Time, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return client.Subscribe(ctx, source_uid, target_uid)
+}
+
+func Unsubscribe(client *Client, source_uid, target_uid string) (time.Time, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return client.Unsubscribe(ctx, source_uid, target_uid)
+}
+
+func TestSetChart(t *testing.T) {
+	conn, cli, err := NewConnectionAndClient("localhost:9090", true)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer conn.Close()
+
+	uid := "337525026058144928"
+	chart := `[ { "id": "0", "rels": { "father": "49b952a3-f42b-4807-b216-bb01364ee444", "mother": "57c348b3-c987-4cce-b48b-e48e7738acb1", "spouses": [ "4a8a329e-65c2-4567-a8e9-4c55ff3a4367" ], "children": [ "2582cfcb-248c-4fc0-8e67-d819b7dc3e6b" ] }, "data": { "firstname": "Мераб", "lastname": "Мамардашвили", "birthday": "", "deathday": "", "avatar": "", "gender": "M", "birthlocation": "Гори" } }, { "id": "49b952a3-f42b-4807-b216-bb01364ee444", "data": { "gender": "M", "firstname": "Константин", "lastname": "Мамардашвили", "birthday": null, "deathday": null, "birthlocation": null }, "rels": { "children": [ "0" ], "spouses": [ "57c348b3-c987-4cce-b48b-e48e7738acb1" ] } }, { "id": "57c348b3-c987-4cce-b48b-e48e7738acb1", "data": { "gender": "F", "firstname": "Ксения", "lastname": "Гарсеванишвили", "birthday": null, "deathday": null, "birthlocation": null }, "rels": { "children": [ "0" ], "spouses": [ "49b952a3-f42b-4807-b216-bb01364ee444" ] } }, { "id": "4a8a329e-65c2-4567-a8e9-4c55ff3a4367", "data": { "gender": "F", "firstname": "Нина", "lastname": "Мордасова", "birthday": null, "deathday": null, "birthlocation": "Москва" }, "rels": { "spouses": [ "0" ], "children": [ "2582cfcb-248c-4fc0-8e67-d819b7dc3e6b" ] } }, { "id": "2582cfcb-248c-4fc0-8e67-d819b7dc3e6b", "data": { "gender": "F", "firstname": "Елена", "lastname": "Мамардашвили", "birthday": null, "deathday": null, "birthlocation": "Москва" }, "rels": { "father": "0", "mother": "4a8a329e-65c2-4567-a8e9-4c55ff3a4367" } } ]`
+
+	r, err := SetChart(cli, uid, chart)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	t.Logf("chart has been updated: %s.\n", r.Local())
+}
+
+func TestGetChart(t *testing.T) {
+	conn, cli, err := NewConnectionAndClient("localhost:9090", true)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer conn.Close()
+
+	uid := "337525026058144928"
+
+	chart, err := GetChart(cli, uid)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	t.Logf("chart is: %s.\n", chart)
+}
+
+/*
+func TestSubscribe(t *testing.T) {
+	conn, cli, err := makeConnection("localhost:9090", true)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer conn.Close()
+
+	uid1 := "337525026058144928"
+	uid2 := "337525026058144929"
+
+	timestamp, err := Subscribe(cli, uid1, uid2)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	t.Logf("subscribed at: %s.\n", timestamp.Local())
+}
+
+func TestUnsubscribe(t *testing.T) {
+	conn, cli, err := makeConnection("localhost:9090", true)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer conn.Close()
+
+	uid1 := "337525026058144928"
+	uid2 := "337525026058144929"
+
+	timestamp, err := Unsubscribe(cli, uid1, uid2)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	t.Logf("unsubscribed at: %s.\n", timestamp.Local())
+}
+*/
